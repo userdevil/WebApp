d8bb45e7be7ab78bc94c881b4f2c157fcb187642
userdevil <62639000+userdevil@users.noreply.github.com>
2021-11-29 15:33:04 +0530

    Update redli_webapp.py

--- redli_webapp.py
+++ redli_webapp.py
@@ -1,14 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
+import sqlite3
+from hashids import Hashids
 import requests
 import os
 import phonenumbers
 from phonenumbers import geocoder
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'this should be a secret random string'
+
+hashids = Hashids(min_length=4, salt=app.config['SECRET_KEY'])
+
 
 @app.route('/')
 def home():
     ip = request.remote_addr
-    return render_template('home.html',count = ip)
+    if not url:
+       flash('The URL is required!')
+       return redirect(url_for('index'))
+       return render_template('home.html',count = ip)
 
 @app.route('/helpline')
 def help():
@@ -37,6 +53,68 @@ def track1():
         crname = carrier.name_for_number(service_provider, "en")
         return render_template('track1.html',location =ch_number , crname = crname,number=number)
 
+
+@app.route('/shorturl', methods=('GET', 'POST'))
+def index():
+    conn = get_db_connection()
+
+    if request.method == 'POST':
+        url = request.form['url']
+
+        if not url:
+            flash('The URL is required!')
+            return redirect(url_for('index'))
+
+        url_data = conn.execute('INSERT INTO urls (original_url) VALUES (?)',(url,))
+        conn.commit()
+        conn.close()
+
+        url_id = url_data.lastrowid
+        hashid = hashids.encode(url_id)
+        short_url = request.host_url + hashid
+
+        return render_template('index.html', short_url=short_url)
+
+    return render_template('index.html')
+@app.route('/<id>')
+def url_redirect(id):
+    conn = get_db_connection()
+
+    original_id = hashids.decode(id)
+    if original_id:
+        original_id = original_id[0]
+        url_data = conn.execute('SELECT original_url, clicks FROM urls'
+                                ' WHERE id = (?)', (original_id,)
+                                ).fetchone()
+        original_url = url_data['original_url']
+        clicks = url_data['clicks']
+
+        conn.execute('UPDATE urls SET clicks = ? WHERE id = ?',
+                     (clicks+1, original_id))
+
+        conn.commit()
+        conn.close()
+        return redirect(original_url)
+    else:
+        flash('Invalid URL')
+        return redirect(url_for('index'))
+
+
+
+@app.route('/stats')
+def stats():
+    conn = get_db_connection()
+    db_urls = conn.execute('SELECT id, created, original_url, clicks FROM urls'
+                           ).fetchall()
+    conn.close()
+
+    urls = []
+    for url in db_urls:
+        url = dict(url)
+        url['short_url'] = request.host_url + hashids.encode(url['id'])
+        urls.append(url)
+
+    return render_template('stats.html', urls=urls)
 
 if __name__=='__main__':
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
